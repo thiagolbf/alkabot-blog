3a290dc674fcc8a2cb98eeb7865a8c5dd0334e16
Thiago Barcelos <thiagolbf@hotmail.com>
2023-03-22 20:10:11 -0300

    feat add header component

---
+++ src/components/Header/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 10px;
+  border-bottom: 2px solid var(--green);
+
+  background-color: var(--lightGreen);
+  font-family: var(--roboto);
+
+  display: flex;
+  justify-content: space-between;
+
+  > h1 {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+    > h1 {
+      margin-bottom: 15px;
+    }
+  }
+
+  > ul {
+    display: flex;
+    font-size: 15px;
+
+    > li {
+      margin: 5px 5px;
+    }
+
+    @media screen and (max-width: 500px) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+`;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Header } from "../components/Header";
+
+export const RoutesComponent = () => {
+  return (
+    <>
+      <Header />
+      <Routes></Routes>
+    </>
+  );
+};
